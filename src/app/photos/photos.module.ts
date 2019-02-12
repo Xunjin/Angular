031c6db50f67abc6397050dbb17ebeda3838b1c7
Xunjin <xunjin.coder@gmail.com>
2019-02-12 19:55:18 -0200

    commit before using CLI to generate stuff

---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { PhotoComponent } from './photo/photo.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [ PhotoComponent ],
+  exports: [ PhotoComponent ],
+  imports: [ HttpClientModule ]
+})
+export class PhotosModule {
+
+}
