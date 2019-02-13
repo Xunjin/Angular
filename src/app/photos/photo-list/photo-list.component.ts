9101c660b6e2452e89a06e85cd99bf35d911f561
Xunjin <xunjin.coder@gmail.com>
2019-02-13 20:21:23 -0200

    Angular 1 of 4 done

--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,27 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { PhotoService } from '../photo/photo.service';
 import { ActivatedRoute } from '@angular/router';
 
+import { Photo } from '../photo/photo';
+import { PhotoService } from '../photo/photo.service';
+
 @Component({
-  selector: 'app-photo-list',
+  selector: 'ap-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
 export class PhotoListComponent implements OnInit {
 
-  photos: any[] = [];
+  photos: Photo[] = [];
+  filter = '';
+  hasMore = true;
+  currentPage = 1;
+  userName = '';
 
   constructor(
-    private photoService: PhotoService,
-    private activatedRoute: ActivatedRoute
+    private activatedRoute: ActivatedRoute,
+    private photoService: PhotoService
     ) { }
 
   ngOnInit(): void {
+    this.userName = this.activatedRoute.snapshot.params.userName;
+    this.photos = this.activatedRoute.snapshot.data['photos'];
 
-      const userName = this.activatedRoute.snapshot.params.userName;
-      this.photoService
-        .listFromUser(userName)
-        .subscribe(photos => this.photos = photos);
   }
 
+  load() {
+    this.photoService.listFromUserPaginated(this.userName,
+      ++this.currentPage).subscribe(photos => {
+        this.filter = '';
+        this.photos = this.photos.concat(photos);
+        if (!photos.length) { this.hasMore = false; }
+      });
+  }
 }
